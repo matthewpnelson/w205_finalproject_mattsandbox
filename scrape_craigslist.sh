0f1ca158b2ef4e01a19e899d5a7024b472f8bf70
matthewpnelson <matthewpeternelson@gmail.com>
2017-04-21 15:27:29 -0600

    playing

---
+++ scrape_craigslist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Delete tmp csv file on local machine & HDFS
+rm /home/w205/craigslist_scrape_tmp/scrape_temp.csv
+hdfs dfs -rm /user/w205/slackbot_static/craigslist_scrape_data/scrape_temp.csv
+
+
+# Run Scraping Script that saves results to a csv file
+python scrape_craigslist.py
+
+# Copy local tmp file to HDFS
+hdfs dfs -put /home/w205/craigslist_scrape_tmp/scrape_temp.csv /user/w205/slackbot_static/craigslist_scrape_data
+
+# Create tmp Hive Table
+hive -f craigslist_tmp_ddl.sql
+
+# Merge temp Table with Persistent Table
+hive -f craigslist_merge.sql
