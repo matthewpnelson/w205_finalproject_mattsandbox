a4fa183d73c4e11c808e134675a7a3a8c69c8a6b
matthewpnelson <matthewpeternelson@gmail.com>
2017-04-21 22:46:23 -0600

    playing

--- evaluation1/run_evaluation.py
+++ evaluation1/run_evaluation.py
@@ -17,7 +17,7 @@ density_of_offstreet_parking = "Low" # Low, Medium, High Density within Xkm (Sel
 # Initialize Spark Context & Select Relevent Data From Hive Tables
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import HiveContext
-from pyspark.sql import SparkSession
+#from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 
 spark = SparkSession.builder.master("yarn").appName("my app").enableHiveSupport().getOrCreate()
