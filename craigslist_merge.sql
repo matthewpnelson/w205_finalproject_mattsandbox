15ac25339eb0a60a2b8102ce63939ebbee60f117
matthewpnelson <matthewpeternelson@gmail.com>
2017-04-21 18:04:29 -0600

    playing

--- craigslist_merge.sql
+++ craigslist_merge.sql
@@ -19,6 +19,7 @@ WITH SERDEPROPERTIES (
 STORED AS TEXTFILE
 LOCATION '/user/w205/slackbot_static/craigslist_scrape_data';
 
+
 -- INSERT INTO craigslist_rentals
 -- (
 -- craigslist_rentals.posted_date,
@@ -46,10 +47,87 @@ LOCATION '/user/w205/slackbot_static/craigslist_scrape_data';
 -- ON craigslist_data_tmp.posting_id = craigslist_rentals.posting_id
 -- WHERE craigslist_rentals.posting_id IS NULL;
 --
-INSERT INTO craigslist_rentals
-SELECT DISTINCT * FROM craigslist_rentals
-UNION ALL
-SELECT DISTINCT * FROM craigslist_data_tmp;
+
+
+-- INSERT INTO unique_rentals
+-- (
+-- craigslist_rentals.posted_date,
+-- craigslist_rentals.geotag,
+-- craigslist_rentals.has_image,
+-- craigslist_rentals.has_map,
+-- craigslist_rentals.posting_id,
+-- craigslist_rentals.name,
+-- craigslist_rentals.price,
+-- craigslist_rentals.url,
+-- craigslist_rentals.location
+-- )
+-- SELECT DISTINCT
+-- craigslist_data_tmp.posted_date,
+-- craigslist_data_tmp.geotag,
+-- craigslist_data_tmp.has_image,
+-- craigslist_data_tmp.has_map,
+-- craigslist_data_tmp.posting_id,
+-- craigslist_data_tmp.name,
+-- craigslist_data_tmp.price,
+-- craigslist_data_tmp.url,
+-- craigslist_data_tmp.location
+-- FROM craigslist_data_tmp
+-- LEFT JOIN craigslist_rentals
+-- ON craigslist_data_tmp.posting_id = craigslist_rentals.posting_id
+-- WHERE craigslist_rentals.posting_id IS NULL;
+--
+-- INSERT INTO unique_rentals
+-- SELECT * FROM craigslist_rentals
+-- UNION
+-- SELECT * FROM craigslist_data_tmp;
+
+DROP TABLE unique_rentals;
+
+CREATE TABLE unique_rentals AS
+SELECT
+unique_rentals.posted_date,
+unique_rentals.geotag,
+unique_rentals.has_image,
+unique_rentals.has_map,
+unique_rentals.posting_id,
+unique_rentals.name,
+unique_rentals.price,
+unique_rentals.url,
+unique_rentals.location
+FROM (
+  SELECT DISTINCT
+  craigslist_rentals.posted_date,
+  craigslist_rentals.geotag,
+  craigslist_rentals.has_image,
+  craigslist_rentals.has_map,
+  craigslist_rentals.posting_id,
+  craigslist_rentals.name,
+  craigslist_rentals.price,
+  craigslist_rentals.url,
+  craigslist_rentals.location
+  FROM craigslist_rentals
+  UNION ALL
+  SELECT DISTINCT
+  craigslist_data_tmp.posted_date,
+  craigslist_data_tmp.geotag,
+  craigslist_data_tmp.has_image,
+  craigslist_data_tmp.has_map,
+  craigslist_data_tmp.posting_id,
+  craigslist_data_tmp.name,
+  craigslist_data_tmp.price,
+  craigslist_data_tmp.url,
+  craigslist_data_tmp.location
+  from craigslist_data_tmp
+) unique_rentals;
+
+
+-- Drop Temporary Copy
+DROP TABLE craigslist_rentals;
+
+
+-- Copy Table
+CREATE TABLE craigslist_rentals AS SELECT * FROM unique_rentals;
+
 
 
 -- INSERT INTO craigslist_rentals
