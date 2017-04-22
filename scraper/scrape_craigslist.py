3a0467df25ad309b84021e687aee9cb6bdc5c74e
matthewpnelson <matthewpeternelson@gmail.com>
2017-04-22 12:18:20 -0600

    playing

--- scraper/scrape_craigslist.py
+++ scraper/scrape_craigslist.py
@@ -1,14 +1,13 @@
 import scraper_settings
-from slackclient import SlackClient
 
 def scrape_craigslist():
 
     from craigslist import CraigslistHousing
 
     # Scrape Craigslist
-    cl = CraigslistHousing(site='sfbay', area=["eby", "sfc", "sby", "nby"], category='apa')
+    cl = CraigslistHousing(site='sfbay', area="sfc", category='apa')
 
-    results = cl.get_results(geotagged=True, limit=100) #do we need to set a reasonable limit?
+    results = cl.get_results(geotagged=True, limit=20) #do we need to set a reasonable limit?
 #sort_by='newest',
 
     # Save to CSV File
